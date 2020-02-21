53f2fd6c59f98e5dee1bd57683544617416be2f1
MarioGomez <mariogomezd.dev@gmail.com>
2020-02-21 10:17:44 -0500

    Test commit of only the java file_this is my commit message

--- src/main/java/cornerTeam/Main.java
+++ src/main/java/cornerTeam/Main.java
@@ -5,6 +5,7 @@ public class Main{
   public static void main(String[]args){
     System.out.println("Hello");
     System.out.println("it Works");
+    System.out.println("savursool: My new Test for commit");
   }
   
   	public void setupAdminPanel() {
